7ec614555a735ea952c68be45f1d2e2807a18892
karan <kshoker1@students.cs.ubc.ca>
2022-03-02 15:05:14 -0800

    Finished phase twoo

--- src/test/model/JsonReaderTest.java
+++ src/test/model/JsonReaderTest.java
@@ -1,13 +1,14 @@
 package model;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import persistence.JsonReader;
 
 import java.io.IOException;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 
 public class JsonReaderTest extends JsonTest {
 
